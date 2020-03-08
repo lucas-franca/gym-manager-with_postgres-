ea5bcaaaa9a056103ca502b8bc6b7c53d83b04af
primobec <lucas.franca.94@hotmail.com>
2020-03-08 18:12:40 -0300

    Implementing models

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,12 +1,9 @@
-const { date } = require("../../lib/utils");
-const db = require("../../config/db");
+const Instructor = require("../models/instructor")
 
 module.exports = {
   index(req, res){
-    db.query(`SELECT * FROM instructors`, function(err, results){
-      if(err) return res.send("Database error!")
-
-      return res.render("instructors/index", {instructors: results.rows})
+    Instructor.all(function(instructors){
+      return res.render("instructors/index", {instructors})
     })
   },
   create(req, res){
@@ -20,33 +17,11 @@ module.exports = {
         return res.send("Por favor, preencha todos os campos")
       }
     }
-  
-    const query = `
-      INSERT INTO instructors (
-        name,
-        avatar_url,
-        gender,
-        services,
-        birth,
-        created_at
-      ) VALUES ($1, $2, $3, $4, $5, $6)
-      RETURNING id
-    `
-    
-    const values = [
-      req.body.name,
-      req.body.avatar_url,
-      req.body.gender,
-      req.body.services,
-      date(req.body.birth).iso,
-      date(Date.now()).iso
-    ]
-  
-    db.query(query, values, function(err, results){
-      if(err) return res.send("Database error!")
 
+    Instructor.create(req.body, function(instructor){
       return res.redirect(`/instructors/${results.rows[0].id}`)
-    })  
+    })
+  
   },
   show(req, res){
     return
