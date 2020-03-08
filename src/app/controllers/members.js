f7fba1cc7bb6abd6da29e03ec52a5ea0b5937bea
primobec <lucas.franca.94@hotmail.com>
2020-03-08 17:11:07 -0300

    First commit

---
+++ src/app/controllers/members.js
@@ -0,0 +1,123 @@
+const { date } = require("../../lib/utils");
+
+exports.index = function(req, res){
+  return res.render("members/index", { members: data.members })
+}
+
+exports.create = function(req,res){
+  return res.render("members/create")
+}
+
+exports.post = function(req, res){
+  const keys = Object.keys(req.body)
+
+  for (key of keys){
+    if(req.body[key] == ""){
+      return res.send("Por favor, preencha todos os campos")
+    }
+  }
+
+  birth = Date.parse(req.body.birth);
+  
+  let id = 1;
+  const lastMember = data.members[data.members.length - 1];
+
+  if(lastMember){
+    id = lastMember.id + 1;
+  }
+
+  data.members.push({
+    id,
+    ... req.body,
+    birth
+  });
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+    if(err) return res.send("Erro ao escrever o arquivo.");
+
+    return res.redirect("/members");
+  });
+
+}
+
+exports.show = function(req,res){
+  const { id } = req.params;
+
+  const foundMember = data.members.find(function(member){
+    return member.id == id;
+  })
+
+  if(!foundMember) return res.send("Membro não encontrado");
+
+  const member = {
+    ... foundMember,
+    birth: date(foundMember.birth).birthDay,
+  }
+
+  return res.render("members/show", { member });
+}
+
+exports.edit = function(req, res){
+  const { id } = req.params;
+
+  const foundMember = data.members.find(function(member){
+    return member.id == id;
+  })
+
+  if(!foundMember) return res.send("Membro não encontrado");
+
+  const member = {
+    ...foundMember,
+    birth: date(foundMember.birth).iso
+  }
+
+  return res.render('members/edit', {member})
+}
+
+exports.put = function(req, res){
+  const { id } = req.body;
+  let index = 0;
+
+  const foundMember = data.members.find(function(member, foundIndex){
+    if(id == member.id){
+      index = foundIndex;
+      return true;
+    }
+  })
+
+  if(!foundMember) return res.send("membro não encontrado");
+
+  const member = {
+    ... foundMember,
+    ... req.body,
+    birth: Date.parse(req.body.birth),
+    id: Number(req.body.id)
+  }
+
+  data.members[index] = member;
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+    if(err) return res.send("Erro ao salvar")
+
+    return res.redirect(`/members/${id}`);
+  })
+
+}
+
+exports.delete = function(req, res){
+  const{ id } = req.body;
+
+  const filteredMembers = data.members.filter(function(member){
+    return member.id != id;
+  })
+
+  data.members = filteredMembers;
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+    if(err){
+      return res.send("Erro ao salvar o arquivo DATA.json")
+    }
+
+    return res.redirect("/members");
+  })
+}
