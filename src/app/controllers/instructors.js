4a39545e6ccfaf0d8400b0bb47d9eae56fd01f79
primobec <lucas.franca.94@hotmail.com>
2020-03-08 17:27:37 -0300

    Shorthands (ES6) implemented in controllers and utils.js

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,125 +1,43 @@
-const { age, date } = require("../utils");
+const { age, date } = require("../../lib/utils");
 
-exports.index = function(req, res){
-  return res.render("instructors/index", { instructors: data.instructors })
-}
+module.exports = {
+  index(req, res){
+    return res.render("instructors/index", { instructors: data.instructors })
+  },
+  create(req, res){
+    return res.render("instructors/create")
+  },
+  post(req, res){
+    const keys = Object.keys(req.body)
 
-exports.create = function(req, res){
-  return res.render("instructors/create")
-}
-
-exports.post = function(req, res){
-  const keys = Object.keys(req.body)
-
-  for (key of keys){
-    if(req.body[key] == ""){
-      return res.send("Por favor, preencha todos os campos")
+    for (key of keys){
+      if(req.body[key] == ""){
+        return res.send("Por favor, preencha todos os campos")
+      }
     }
-  }
+  
+    let {avatar_url, birth, name, services, gender} = req.body;
+  
+    return 
+  },
+  show(req, res){
+    return
+  },
+  edit(req, res){
+    return
+  },
+  put(req, res){
+    const keys = Object.keys(req.body)
 
-  let {avatar_url, birth, name, services, gender} = req.body;
-
-  birth = Date.parse(birth);
-  const created_at = Date.now();
-  const id = Number(data.instructors.length + 1);
-
-  data.instructors.push({
-    id,
-    avatar_url,
-    name,
-    birth,
-    gender,
-    services,
-    created_at
-  });
-
-  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
-    if(err) return res.send("Erro ao escrever o arquivo.");
-
-    return res.redirect("/instructors");
-  });
-}
-
-exports.show = function(req,res){
-  const { id } = req.params;
-
-  const foundInstructor = data.instructors.find(function(instructor){
-    return instructor.id == id;
-  })
-
-  if(!foundInstructor) return res.send("Instrutor não encontrado");
-
-  const instructor = {
-    ... foundInstructor,
-    age: age(foundInstructor.birth),
-    services: foundInstructor.services.split(","),
-    created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at)
-  }
-
-  return res.render("instructors/show", { instructor });
-}
-
-exports.edit = function(req, res){
-  const { id } = req.params;
-
-  const foundInstructor = data.instructors.find(function(instructor){
-    return instructor.id == id;
-  })
-
-  if(!foundInstructor) return res.send("Instrutor não encontrado");
-
-  const instructor = {
-    ...foundInstructor,
-    birth: date(foundInstructor.birth).iso
-  }
-
-  return res.render('instructors/edit', {instructor})
-}
-
-exports.put = function(req, res){
-  const { id } = req.body;
-  let index = 0;
-
-  const foundInstructor = data.instructors.find(function(instructor, foundIndex){
-    if(id == instructor.id){
-      index = foundIndex;
-      return true;
-    }
-  })
-
-  if(!foundInstructor) return res.send("Instrutor não encontrado");
-
-  const instructor = {
-    ... foundInstructor,
-    ... req.body,
-    birth: Date.parse(req.body.birth),
-    id: Number(req.body.id)
-  }
-
-  data.instructors[index] = instructor;
-
-  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
-    if(err) return res.send("Erro ao salvar")
-
-    return res.redirect(`/instructors/${id}`);
-  })
-
-}
-
-exports.delete = function(req, res){
-  const{ id } = req.body;
-
-  const filteredInstructors = data.instructors.filter(function(instructor){
-    return instructor.id != id;
-  })
-
-  data.instructors = filteredInstructors;
-
-  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
-    if(err){
-      return res.send("Erro ao salvar o arquivo DATA.json")
+    for (key of keys){
+      if(req.body[key] == ""){
+        return res.send("Por favor, preencha todos os campos")
+      }
     }
 
-    return res.redirect("/instructors");
-  })
+    return
+  },
+  delete(req, res){
+    return
+  }
 }
